0f84958d12ec6307fa41954fb100ef2605f37a56
Jerry Britto <jbritto2004@gmail.com>
2023-11-21 20:53:02 +0530

    update

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ dotenv.config({
     path:"./env"
 })
 app.use(express.json())
-app.use(cors({
-    origin:"*",
-    methods:["GET","POST"],
-    credentials:true
-}))
+app.use(cors())
  app.use("/api",router)
 app.use(express.urlencoded({extended:true}))
 connectToDB()
