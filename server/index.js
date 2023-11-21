56472bf4470493fbe2668fd56a953120e6873afc
Jerry Britto <jbritto2004@gmail.com>
2023-11-21 18:51:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 })
 app.use(express.json())
 app.use(cors({
-    origin: 'https://assignment-frontend-sigma.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   }));
