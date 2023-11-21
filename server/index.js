20d279bd0559342d8af8c3a58fa5c261318e9f09
Jerry Britto <jbritto2004@gmail.com>
2023-11-21 17:40:12 +0530

    Updated server

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 app.use(express.json())
 app.use(cors(
     {
-        origin:["https://deploy-mern-1whq.vercel.app"],
+        origin:["https://deploy-mern-frontend-kohl.vercel.app/"],
         "credentials":true
     }
 ))
