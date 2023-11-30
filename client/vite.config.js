63c14416145b74c67e868de9ca3eabf4172e03d3
Jerry Britto <jbritto2004@gmail.com>
2023-11-30 20:01:59 +0530

    done coding

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-        target: "https://intern-project-blush.vercel.app/",
+        target: "http://localhost:4000/",
         changeOrigin: true,
         secure: false,
       },
