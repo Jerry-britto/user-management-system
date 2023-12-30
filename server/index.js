88646ee3d401e40b5d2287efbd22be3cab3a9b23
Jerry Britto <jbritto2004@gmail.com>
2023-12-30 16:56:45 +0530

    fixed cors errors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,16 @@ dotenv.config({
     path:"./env"
 })
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:"https://intern-project-zrcr.vercel.app/",
+        credentials:true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
+        allowedHeaders: "Content-Type,Authorization", 
+        preflightContinue: false, 
+        optionsSuccessStatus: 204,
+    }
+))
  app.use("/api",router)
 app.use(express.urlencoded({extended:true}))
 connectToDB()
