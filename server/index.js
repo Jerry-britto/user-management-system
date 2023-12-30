e76d94147d4766c39612418837500b4ddf1fcd07
Jerry Britto <jbritto2004@gmail.com>
2023-12-30 17:03:29 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -9,16 +9,7 @@ dotenv.config({
     path:"./env"
 })
 app.use(express.json())
-app.use(cors(
-    {
-        origin:"https://intern-project-zrcr.vercel.app/",
-        credentials:true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
-        allowedHeaders: "Content-Type,Authorization", 
-        preflightContinue: false, 
-        optionsSuccessStatus: 204,
-    }
-))
+app.use(cors())
  app.use("/api",router)
 app.use(express.urlencoded({extended:true}))
 connectToDB()
