8ba7e5283e81b3658808e91612e59d564e43bd41
Jerry Britto <jbritto2004@gmail.com>
2023-12-31 18:55:35 +0530

    Fixed some bugs and design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,80 +1,93 @@
 import React, { useState } from "react";
-import { useDispatch,useSelector } from "react-redux";
-import { searchUser,setChange,emptyGroup } from "../store/slice";
+import { useDispatch, useSelector } from "react-redux";
+import { searchUser, setChange, emptyGroup } from "../store/slice";
 import { NavLink } from "react-router-dom";
-import Axios from "axios"
+import Axios from "axios";
 function Navbar() {
   const dispatch = useDispatch();
-  const change = useSelector(state=>state.clickable)
-  const group = useSelector(state=>state.group)
-  const users = useSelector(state=>state.users)
-  // console.log("Groups ",group);
+  const change = useSelector((state) => state.clickable);
+  const group = useSelector((state) => state.group);
+  const users = useSelector((state) => state.users);
 
-  let [val,setVal]=useState("")
-  const submitName = (e)=>{
+  let [val, setVal] = useState("");
+  const submitName = (e) => {
     e.preventDefault();
-    if(!val){
-      dispatch(searchUser(""))
+    if (!val) {
+      dispatch(searchUser(""));
+    } else {
+      dispatch(searchUser(val.trim()));
     }
-    else{
-      dispatch(searchUser(val))
-    }
-  }
+  };
 
-  const createGroup = async()=>{
-    if(group.length===0){
-      alert("Kindly select the your group members")
+  const createGroup = async () => {
+    if (group.length === 0) {
+      alert("Kindly select the your group members");
       return;
     }
-    if(group.length > 1){
+    if (group.length > 1) {
       //checking group member domain and available status
 
       //retrieving users data
-      const usersInGroup = users.filter(user => group.includes(user._id));
-      const domains = new Set(usersInGroup.map((user)=>user.domain)) //extracting unique domains
-      const availableStatus = new Set(usersInGroup.map((user)=>user.available));
+      const usersInGroup = users.filter((user) => group.includes(user._id));
+      const domains = new Set(usersInGroup.map((user) => user.domain)); //extracting unique domains
+      const availableStatus = new Set(
+        usersInGroup.map((user) => user.available)
+      );
 
-      if(usersInGroup.length!=domains.size || usersInGroup.length!=availableStatus.size){
-        alert("Users in the group must have unique domains and available status");
-        dispatch(emptyGroup())
+      if (
+        usersInGroup.length != domains.size ||
+        usersInGroup.length != availableStatus.size
+      ) {
+        alert(
+          "Users in the group must have unique domains and available status"
+        );
+        dispatch(emptyGroup());
         return;
       }
     }
 
-    let nums = [2,4,6,8,10,12,14,16,69,59,49,9,19,29,39,49,18,20];
+    let nums = [
+      2, 4, 6, 8, 10, 12, 14, 16, 69, 59, 49, 9, 19, 29, 39, 49, 18, 20,
+    ];
     const randomIndex = Math.floor(Math.random() * nums.length);
-    const id = Math.floor(Math.random()*1000) + nums[randomIndex];
+    const id = Math.floor(Math.random() * 1000) + nums[randomIndex];
     const body = {
       id,
-      members:group
-    }
+      members: group,
+    };
 
-    
-    const res = await Axios.post("https://intern-project-blush.vercel.app/api/team",body)
-    alert("Group id "+body.id.toString())
-    dispatch(emptyGroup())
-
-  }
+    const res = await Axios.post(
+      "https://intern-project-blush.vercel.app/api/team",
+      body
+    );
+    alert("Group id " + body.id.toString());
+    dispatch(emptyGroup());
+  };
   return (
-    <div className="bg-black w-auto mr-0 p-2 text-white">
+    <div className="bg-black w-auto p-2 text-white">
       <ul className="flex justify-around w-full">
-        <li className="cursor-pointer hover:shadow-2xl" onClick={()=>dispatch(setChange())}>
-          <button className="bg-red-500 p-1.5 rounded-md ">
-          {change?<li onClick={createGroup}>Save Changes</li>:"Create Group"}
+        <li
+          className="cursor-pointer hover:shadow-2xl"
+          onClick={() => dispatch(setChange())}
+        >
+          <button className="bg-red-500 p-1 md:p-2 rounded-md mx-4">
+            {change ? (
+              <li onClick={createGroup}>Save Changes</li>
+            ) : (
+              "Create Group"
+            )}
           </button>
-          </li>
-        <li className="cursor-pointer hover:text-orange-500">
-          <NavLink to="/showgroups">
-            View Group
-          </NavLink>
         </li>
-        <li>
+        <li className="cursor-pointer hover:text-orange-500 md:my-1">
+          <NavLink to="/showgroups">View Group</NavLink>
+        </li>
+        <li className="md:my-auto my-1">
           <form className="flex" onSubmit={submitName}>
             <input
               type="text"
               placeholder="Search user"
               value={val}
-              onChange={(e)=>setVal(e.target.value)}
+              onChange={(e) => setVal(e.target.value)}
               className="p-1.5 w-full active:border active:border-blue-500 text-black rounded-md"
             />
             <input
