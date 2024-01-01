81b6980e3b71615f8a347c31bfb158af3b20b55b
Jerry Britto <jbritto2004@gmail.com>
2024-01-01 21:52:37 +0530

    Made it more responisve and added a custom loader

--- client/src/components/Container/Container.jsx
+++ client/src/components/Container/Container.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card";
 import Axios from "axios";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../store/slice";
+import Loader from "../Loader";
 function Container() {
   const [data, setData] = useState([]);
   const pageno = useSelector((state) => state.page);
@@ -11,7 +12,7 @@ function Container() {
   const gender = useSelector((state) => state.gender);
   const available = useSelector((state) => state.available);
   const dispatch = useDispatch();
-
+  const [loading,setLoading] = useState(true);
   const change = useSelector((state) => state.clickable);
   async function loadData() {
     try {
@@ -20,37 +21,47 @@ function Container() {
       );
       setData(res.data);
       dispatch(setUser(res.data));
-      
     } catch (error) {
       console.log(error);
     }
   }
   useEffect(() => {
+    setLoading(true);
+    const loaderTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
     loadData();
+    return () => {
+      // Clear the timeout if the component unmounts or the data is loaded before 3 seconds
+      clearTimeout(loaderTimeout);
+    };
+
   }, [pageno, name, domain, gender, available]);
   return (
     <>
-      {data.length === 0 ? (
-        <h1 className="text-center text-4xl">No users found</h1>
-      ) : (
-        ""
-      )}
-      <div className="flex flex-wrap w-full ">
-        {data.map((user) => (
-          <Card
-            id={user._id}
-            icon={change}
-            key={user.id}
-            avatar={user.avatar}
-            first_name={user.first_name}
-            last_name={user.last_name}
-            email={user.email}
-            gender={user.gender}
-            domain={user.domain}
-            available={user.available}
-          />
-        ))}
-      </div>
+      {loading?(
+        <Loader/>
+      ):
+      data.length === 0 ?
+        <h1 className="md:mx-auto md:my-36 md:text-4xl text-3xl m-5">USER NOT FOUND</h1>
+       : 
+        <div className="flex flex-wrap w-full ">
+          {data.map((user) => (
+            <Card
+              id={user._id}
+              icon={change}
+              key={user.id}
+              avatar={user.avatar}
+              first_name={user.first_name}
+              last_name={user.last_name}
+              email={user.email}
+              gender={user.gender}
+              domain={user.domain}
+              available={user.available}
+            />
+          ))}
+        </div>
+      }
     </>
   );
 }
---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../components/loader.css';
+function Loader() {
+  return (
+    <div className='loader md:mx-auto md:my-5 mx-5 my-2'></div>
+  )
+}
+
+export default Loader
